905a235d47dc27cd57ff1bd006314eaca7e0621a
Matheus Marçal <neew.marcal@gmail.com>
2026-03-14 22:28:20 -0300

    .

--- ToDoProject.TESTE/Servicos/TarefaServicoTeste.cs
+++ ToDoProject.TESTE/Servicos/TarefaServicoTeste.cs
@@ -1,23 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 using ToDoProject.DOMINIO.Interfaces;
 using ToDoProject.TESTE.Configuração;
+using Microsoft.Extensions.DependencyInjection;
+using ToDoProject.TESTE.repositorioMock;
 
 namespace ToDoProject.TESTE.Servicos;
 
 public class TarefaServicoTeste : TesteBase
 {
-    private readonly ITarefaRepositorio _tarefaRepositorio;
-    
-    public TarefaServicoTeste()
+    [Fact]
+    public void Deve_retornar_lista_vazia_quando_nao_existir_tarefas()
     {
-        
+        //arrange
+        var repositorio = serviceProvider.GetService<ITarefaRepositorio>();
+
+        //act
+        var resultado = repositorio.ObterTodos();
+
+        //assert
+        Assert.Empty(resultado);
     }
-
-    //Arrange
-
-    //Act
-    
-    //Assert
 }
